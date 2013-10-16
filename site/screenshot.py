6340500c3018a4ded7618f57c2ab2bf538be0b31
Jean-Baptiste (Rorist) Aubort <aubort.jeanbaptiste@gmail.com>
2013-10-16 22:53:24 +0200

    allow 4 images per hour max, the last image is overwritten

--- site/screenshot.py
+++ site/screenshot.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-import requests
+import requests, arrow
 
 SAVE = './img/snap'
 URL = 'http://62.220.135.212/jpg/image.jpg'
 
+#Save 1 image per 15 minute max
+#~1.5G per year of image max
+
+minutes = int(round(int(arrow.now().format('mm'))/15)*15)
+SAVE += '/%s%s.jpg' % (arrow.now().format('YYYYMMDDHH'), minutes)
 r = requests.get(URL)
-f = open('%s/image.jpg' % SAVE, 'w+')
+f = open(SAVE, 'w+')
 f.write(r.content)
 f.close()
