03c9c2b29725885e191632b43bac9fafdb18e430
Jean-Baptiste (Rorist) Aubort <aubort.jeanbaptiste@gmail.com>
2013-10-16 22:40:10 +0200

    Hum ok the ip camera already has still image...

--- site/screenshot.py
+++ site/screenshot.py
@@ -3,25 +3,9 @@
 import requests
 
 SAVE = './img/snap'
-URL = 'http://62.220.135.212/mjpg/video.mjpg'
-start = False
-image = []
+URL = 'http://62.220.135.212/jpg/image.jpg'
 
-# Extract an image from the stream
-r = requests.get(URL, stream=True)
-bound = r.headers['content-type'][36:]
-for line in r.iter_lines():
-    if line:
-        if start == True:
-            image.append(line)
-        if line == bound:
-            if start == True:
-                break
-            start = True
-
-
-# Clean and write image data
-res = ''.join(image[2:-1])
+r = requests.get(URL)
 f = open('%s/image.jpg' % SAVE, 'w+')
-f.write(res)
+f.write(r.content)
 f.close()
